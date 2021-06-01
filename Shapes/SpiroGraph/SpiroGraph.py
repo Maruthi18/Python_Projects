efb1d4f04e66dd83a3d18df5d8f81b21ec7188de
Maruthi18 <syavemaruthi18@gmail.com>
2021-06-02 01:25:21 +0530

    SpiroGraph Using Turtle Added OnClick Function

--- Shapes/SpiroGraph/SpiroGraph.py
+++ Shapes/SpiroGraph/SpiroGraph.py
@@ -1,6 +1,8 @@
 from turtle import Turtle, Screen
 import turtle as t
 
+screen = Screen()
+
 
 
 t.bgcolor("black") # background color
@@ -9,8 +11,10 @@ t.speed(100) # Drawing Speed
 
 colors = ["red", "green", "blue", "brown", "cyan", "aquamarine", "burlywood", "yellow"] # colors list to iterate
 
-for _ in range(6):
+for _ in range(20):
 	for color in colors: # iterate colors list one by one
 		t.color(color) # select the color from colors list
 		t.circle(100) # Draw a circle with radian 100
 		t.right(10) # outside the circle is 10 degrees
+
+screen.exitonclick()
